4901b90c6516a4a640999808430486e52569016a
Naaz-Rampure <98505051+Naaz-Rampure@users.noreply.github.com>
2022-03-17 11:10:56 +0530

    Add files via upload

---
+++ Question_NO_1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+void main()
+{
+	
+	char Rbuff[1000];
+	
+	int fd1 = open("input.txt", O_RDONLY, 777);			
+	
+	int fd2 = open("output.txt",O_RDWR, 777);		
+	int len;
+	
+	
+	read(fd1, Rbuff, 200);
+	
+	printf("data read = %s\n",Rbuff);
+	
+	
+	len = write(fd2, Rbuff, 200);
+	
+	printf("data written = %d \n", len);
+	
+	
+	
+	close(fd2);
+	close(fd1);
+	
+}
